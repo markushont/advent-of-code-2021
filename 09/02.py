9b2eb389919245fd36a99d5c8eee99d1f987223b
Markus Silberstein Hont <markus@silberstein.nu>
2021-12-11 13:29:57 +0100

    Fix 09_02

--- 09/02.py
+++ 09/02.py
@@ -11,11 +11,13 @@ class Point:
 class Cave:
     def __init__(self, points):
         self.points = {r:{c: Point((r, c), points[r][c]) for c in range(len(points[r]))} for r in range(len(points)) }
+        self.n_rows = len(self.points.keys())
+        self.n_cols = len(self.points[0].keys())
 
-        last_in_col = self.n_cols() - 1
+        last_in_col = self.n_cols - 1
         for r in range(last_in_col+1):
             row = self.points[r]
-            last_in_row = self.n_rows() - 1
+            last_in_row = self.n_rows - 1
             for c in range(last_in_row+1):
                 p = row[c].height
                 if r != 0 and self.points[r-1][c].height <= p:
@@ -30,14 +32,8 @@ class Cave:
                     self.points[r][c].is_low = True
                     self.points[r][c].basin_coord = (r, c)
 
-    def n_cols(self):
-        return len(self.points[0])
-
-    def n_rows(self):
-        return len(self.points)
-
     def get_right(self, r, c):
-        return None if c >= self.n_cols() else self.points[r][c+1]
+        return None if c >= self.n_cols-1 else self.points[r][c+1]
     
     def get_left(self, r, c):
         return None if c == 0 else self.points[r][c-1]
@@ -46,60 +42,52 @@ class Cave:
         return None if r == 0 else self.points[r-1][c]
 
     def get_down(self, r, c):
-        return None if r >= self.n_rows() else self.points[r+1][c]
+        return None if r >= self.n_rows-1 else self.points[r+1][c]
 
     def get_low_points(self):
         ret = []
         for r in range(len(self.points)):
-            for j in range(len(self.points[r])):
+            for c in range(len(self.points[r])):
                 if self.points[r][c].is_low:
-                    ret.append((r, c))
+                    ret.append(self.points[r][c])
         return ret
 
-def find_basin_coord(cave, r, c):
-    if cave.points[r][c].basin_coord or cave.points[r][c].is_low:
-        return cave.points[r][c].basin_coord
-    elif cave.points[r][c].height == 9:
-        return None
+    def get_surrounding_points(self, r, c):
+        return {
+            self.get_right(r, c),
+            self.get_up(r, c),
+            self.get_left(r, c),
+            self.get_down(r, c)
+        }
 
-    if r != 0:
-        coords = find_basin_coord(cave, r-1, c)
-        if coords:
-            return coords
-    if r != len(cave.points) -1:
-        coords = find_basin_coord(cave, r+1, c)
-        if coords:
-            return coords
-    if c != 0:
-        coords = find_basin_coord(cave, r, c-1)
-        if coords:
-            return coords
-    if c != len(cave.points[r]) - 1:
-        coords = find_basin_coord(cave, r, c+1)
-        if coords:
-            return coords
-
-    return None
+    def get_points_in_basin(self, basin_r, basin_c):
+        ret = set()
+        for r in range(self.n_rows):
+            for c in range(self.n_cols):
+                if self.points[r][c].basin_coord == (basin_r, basin_c):
+                    ret.add(self.points[r][c])
+        return ret
 
 cave = Cave(cdata)
 
-basins = [[p] for p in cave.get_low_points()]
-
+basins = []
 for b in cave.get_low_points():
-    row_it = b.basin_coord[0]
-    col_it = b.basin_coord[1]
+    to_check = {b: cave.get_surrounding_points(b.coord[0], b.coord[1])}
 
-    prev = b
-    curr = cave.get_right(row_it, col_it)
-    while curr:
-        if prev.height >= curr.height:
-            break
+    check_next = to_check
+    while check_next:
+        check_next = {}
+        for base, points in to_check.items():
+            for p in points:
+                if not p:
+                    continue
+                if p.height > base.height and p.height < 9:
+                    p.basin_coord = base.basin_coord
+                    check_next.update({p: {x for x in cave.get_surrounding_points(p.coord[0], p.coord[1]) if x and not x.basin_coord}})
+        to_check = check_next
 
 
-for r in range(cave.n_rows()):
-    for c in range(cave.n_cols()):
-        basin_coord = find_basin_coord(cave, r, c)
-        if basin_coord not in basin_coords:
-            basin_coords.append(basin_coord)
+    basins.append(cave.get_points_in_basin(b.coord[0], b.coord[1]))
 
-print(basin_coords)
+basins_sorted = sorted([len(basin) for basin in basins], reverse=True)
+print(basins_sorted[0]*basins_sorted[1]*basins_sorted[2])
